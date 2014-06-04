fed64bd577cfadc32982dabef2bf45b55e9f37a9
Matthew Thode <mthode@mthode.org>
2014-06-04 12:58:34 -0500

    add user support (depends)

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description 'Provides a full Tomcat stack'
 
 version '0.1.0'
 
-depends 'rackspace_iptables', '>= 1.3'
+depends 'rackspace_iptables', '>= 1.3.1'
+depends 'user', '>= 0.3.0'
 depends 'chef-sugar'
