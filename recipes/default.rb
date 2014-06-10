c36e471223428ffb22c4d6083b5d9208e8735b2c
Marco A. Morales <marcoamorales@gmail.com>
2014-06-10 15:07:47 -0500

    refixing merge

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,3 @@
-include_recipe 'platformstack::iptables'
-include_recipe 'platformstack::ntp'
-include_recipe 'platformstack::openssh'
-include_recipe 'platformstack::timezone'
-
 if node['platformstack']['enable_postfix'] == true
   include_recipe 'postfix'
 end
