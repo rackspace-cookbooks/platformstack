c277a5e25e6732903a149d47c297209080c92a88
Matthew Thode <mthode@mthode.org>
2014-06-10 11:00:18 -0500

    better OS agnostic support in monitoring

--- recipes/monitors.rb
+++ recipes/monitors.rb
@@ -1,9 +1,22 @@
-apt_repository 'monitoring' do
-  uri 'http://stable.packages.cloudmonitoring.rackspace.com'
-  distribution "#{node['lsb']['release']}-#{node['lsb']['codename']}-x86_64"
-  components ['main']
-  keyserver 'keyserver.ubuntu.com'
-  key 'D05AB914'
+case node['platform_family']
+when 'debian'
+  apt_repository 'monitoring' do
+    uri 'https://stable.packages.cloudmonitoring.rackspace.com'
+    distribution "#{node['platform']}-#{node['lsb']['codename']}-x86_64"
+    components ['main']
+    keyserver 'keyserver.ubuntu.com'
+    key 'D05AB914'
+    action :create
+  end
+when 'rhel'
+  yum_repository 'monitoring' do
+    description 'Rackspace Cloud Monitoring agent repo'
+    baseurl "https://stable.packages.cloudmonitoring.rackspace.com/#{node['platform']}-#{node['lsb']['release']}-x86_64"
+    gpgkey "https://monitoring.api.rackspacecloud.com/pki/agent/#{node['platform']}-#{node['lsb']['release']}.asc"
+    enabled true
+    gpgcheck true
+    action :create
+  end
 end
 
 package 'rackspace-monitoring-agent' do
