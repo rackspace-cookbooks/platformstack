41133504cf700aeadca475b27d77f4e262594315
Matthew Thode <mthode@mthode.org>
2014-06-10 10:31:37 -0500

    now with more correct syntax

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,14 @@
-include_recipe 'platformstack::iptables'
-include_recipe 'platformstack::ntp'
-include_recipe 'platformstack::openssh'
-include_recipe 'platformstack::timezone'
+log 'run the default stuff last' do
+  level :debug
+  notifies :create, 'rubt_block[platformstack]', :delayed
+end
+
+ruby_block 'platformstack' do
+  block do
+    include_recipe 'platformstack::iptables'
+    include_recipe 'platformstack::ntp'
+    include_recipe 'platformstack::openssh'
+    include_recipe 'platformstack::timezone'
+  end
+  action :create
+end
