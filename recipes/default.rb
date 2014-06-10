b58f9b35d7b4c9358430cc4a0c945e2b485135ce
Matthew Thode <mthode@mthode.org>
2014-06-10 11:06:07 -0500

    more testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,21 @@
 log 'run the default stuff last' do
   level :debug
-#  notifies :create, 'ruby_block[platformstack]', :delayed
+  notifies :create, 'ruby_block[platformstack]', :delayed
 end
 
-include_recipe 'platformstack::iptables'
-include_recipe 'platformstack::ntp'
-include_recipe 'platformstack::openssh'
-include_recipe 'platformstack::timezone'
-include_recipe 'platformstack::logstash_rsyslog'
+ruby_block 'platformstack' do
+  block do
+    Chef::DSL::IncludeRecipe('platformstack::iptables')
+    Chef::DSL::IncludeRecipe('platformstack::ntp')
+    Chef::DSL::IncludeRecipe('platformstack::openssh')
+    Chef::DSL::IncludeRecipe('platformstack::timezone')
+    Chef::DSL::IncludeRecipe('platformstack::logstash_rsyslog')
+  end
+end
+#include_recipe 'platformstack::iptables'
+#include_recipe 'platformstack::ntp'
+#include_recipe 'platformstack::openssh'
+#include_recipe 'platformstack::timezone'
+#include_recipe 'platformstack::logstash_rsyslog'
+#include_recipe 'platformstack::monitors'
+#include_recipe 'platformstack::newrelic'
