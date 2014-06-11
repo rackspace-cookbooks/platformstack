cb677136c709af595e23d27cda3376c3dd131148
Matthew Thode <mthode@mthode.org>
2014-06-11 09:51:17 -0500

    removing rackops
    also moving postfix to the correct include_recipe position

--- attributes/rackops.rb
+++
@@ -1,3 +0,0 @@
-node.default['platformstack']['rackops']['enabled'] = true
-node.default['platformstack']['rackops']['editor']['default'] = '/usr/bin/vim.basic'
-node.default['platformstack']['rackops']['templates']['editor-env-var'] = 'platformstack'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,6 @@
 node.default['authorization']['sudo']['include_sudoers_d'] = true
 node.default['chef-client']['log_file'] = '/var/log/chef/client.log'
 
-if node['platformstack']['enable_postfix'] == true
-  include_recipe 'postfix'
-end
-
 log 'run the default stuff last' do
   level :debug
   notifies :create, 'ruby_block[platformstack]', :delayed
@@ -17,15 +13,10 @@ ruby_block 'platformstack' do
     run_context.include_recipe('platformstack::timezone')
     run_context.include_recipe('platformstack::logstash_rsyslog')
     run_context.include_recipe('platformstack::monitors')
+    if node['platformstack']['enable_postfix'] == true
+      run_context.include_recipe('postfix::default')
     unless node['newrelic']['license'].nil?
       run_context.include_recipe('platformstack::newrelic')
-    end
-    if node['platformstack']['rackops']['enabled'] == true
-      run_context.include_recipe('motd-tail::default')
-      run_context.include_recipe('ohai::default')
-      run_context.include_recipe('sudo::default')
-      run_context.include_recipe('user::default')
-      run_context.include_recipe('platformstack::rackops')
     end
     # run this last because if feels so good
     run_context.include_recipe('platformstack::iptables')
