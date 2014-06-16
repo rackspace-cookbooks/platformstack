c2fd2390e77801c1cdff47240012057362672d7f
BK Box <bk.box@rackspace.com>
2014-06-16 15:49:19 -0500

    Revert "fixing attribute name"
    This reverts commit 7af33db9a7db1ce95ed4d5bcf06528ccb9bcb219.

--- recipes/default.rb
+++ recipes/default.rb
@@ -52,7 +52,7 @@ ruby_block 'platformstack' do
     if node['rackspace']['cloudbackup']['enabled'] == true
       run_context.include_recipe('rackspace_cloudbackup::default')
     end
-    if node['platformstack']['cloud_monitoring']['enabled'] == true
+    if node['rackspace']['cloud_monitoring']['enabled'] == true
       run_context.include_recipe('platformstack::monitors')
     end
     # run this last because if feels so good
