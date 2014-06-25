2daea035591b4640e48237a869ed7084a301c68a
Hart Hoover <hart.hoover@rackspace.com>
2014-06-25 14:48:28 -0500

    Add RackConnect Logic to IPTables
    - default is false (no RC)
    - if rackconnect is true, add a RC user to sudoers and don't set any iptables rules

--- recipes/iptables.rb
+++ recipes/iptables.rb
@@ -18,19 +18,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-include_recipe 'rackspace_iptables::default'
-if node['openssh']['server'].attribute?('port')
-  ssh_port = node['openssh']['server']['port']
+if node['platformstack']['rackconnect'] == true
+  sudo 'rackconnect' do
+    user 'rackconnect'
+    nopasswd true
+  end
 else
-  ssh_port = '22'
-end
+  include_recipe 'rackspace_iptables::default'
 
-if node['platformstack']['iptables']['allow_ssh_from_world'] == true
-  add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{ssh_port} -j ACCEPT", 9999, 'Allow ssh from the world')
-end
+  if node['openssh']['server'].attribute?('port')
+    ssh_port = node['openssh']['server']['port']
+  else
+    ssh_port = '22'
+  end
 
-# Return
-add_iptables_rule('INPUT', '-i lo -j ACCEPT', 3, 'Loopback')
-add_iptables_rule('INPUT', '-m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT', 2, 'Allow establihsed')
-add_iptables_rule('INPUT', '-s 0.0.0.0/0 -j REJECT', 1, 'Drop not allowed')
+  if node['platformstack']['iptables']['allow_ssh_from_world'] == true
+    add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{ssh_port} -j ACCEPT", 9999, 'Allow ssh from the world')
+  end
+
+  add_iptables_rule('INPUT', '-i lo -j ACCEPT', 3, 'Loopback')
+  add_iptables_rule('INPUT', '-m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT', 2, 'Allow establihsed')
+  add_iptables_rule('INPUT', '-s 0.0.0.0/0 -j REJECT', 1, 'Drop not allowed')
+end
