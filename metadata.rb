d34080fc1247a1ba98e7e4f68ccb9ed91921290d
Hart Hoover <hart.hoover@rackspace.com>
2014-07-02 13:46:50 -0500

    Install monitoring agent package, bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'Provides a full Tomcat stack'
 
-version '0.2.3'
+version '0.2.4'
 
 depends 'apt'
 depends 'auto-patch'
