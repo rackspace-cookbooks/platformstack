12490a2ca70fcfcbdfb3a3aae33c1f80cfe25010
Matthew Thode <mthode@mthode.org>
2014-07-21 10:53:41 -0500

    more filesystems exempted and another check for #79

--- attributes/cloud_monitoring.rb
+++ attributes/cloud_monitoring.rb
@@ -33,9 +33,15 @@ default['platformstack']['cloud_monitoring']['filesystem']['period'] = 60
 default['platformstack']['cloud_monitoring']['filesystem']['timeout'] = 30
 default['platformstack']['cloud_monitoring']['filesystem']['crit'] = 90
 default['platformstack']['cloud_monitoring']['filesystem']['warn'] = 80
+default['platformstack']['cloud_monitoring']['filesystem']['cookbook'] = 'platformstack'
 
 node['filesystem'].each do |key, data|
-  next if data['percent_used'].nil? || data['fs_type'] == 'tmpfs'
+  next if data['percent_used'].nil? || data['fs_type'] == ('tmpfs' || 'devtmpfs' ||
+                                                           'devpts' || 'proc' ||
+                                                           'mqueue' || 'cgroup' ||
+                                                           'efivars' || 'sysfs' ||
+                                                           'sys' || 'securityfs' ||
+                                                           'configfs' || 'fusectl')
   default['platformstack']['cloud_monitoring']['filesystem']['target'][key] = data['mount']
 end
 
