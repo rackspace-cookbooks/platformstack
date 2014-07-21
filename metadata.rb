6673858d2a5adfc1c60d91718a037813c78f00ff
Matthew Thode <mthode@mthode.org>
2014-07-21 12:51:41 -0500

    define which OS's we support

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description 'Provides a full Tomcat stack'
 
 version '0.3.2'
 
+%w(ubuntu debian redhat centos).each do |os|
+  supports os
+end
+
 depends 'apt'
 depends 'auto-patch'
 depends 'chef-client'
