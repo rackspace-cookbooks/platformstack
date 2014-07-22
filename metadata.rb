198ce06a5e6172f2f951848003d7648b391da74f
Matthew Thode <mthode@mthode.org>
2014-07-22 16:08:20 -0500

    first release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'Provides a full Tomcat stack'
 
-version '0.3.2'
+version '1.0.0'
 
 %w(ubuntu debian redhat centos).each do |os|
   supports os
