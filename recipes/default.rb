4801058e7520fe970865a716707dd54de6665be0
Troy C <rstrox@users.noreply.github.com>
2014-09-17 06:27:32 -0500

    Initial integration of Consul into platformstack

--- recipes/default.rb
+++ recipes/default.rb
@@ -53,6 +53,7 @@ ruby_block 'platformstack' do # ~FC014
     run_context.include_recipe('rsyslog::client') if node['platformstack']['logstash_rsyslog']['enabled'] == true
     run_context.include_recipe('client-rekey') if node['platformstack']['client_rekey']['enabled'] == true
     run_context.include_recipe('omnibus_updater') if node['platformstack']['omnibus_updater']['enabled'] == true
+    run_context.include_recipe('consul') if node['platformstack']['consul']['enabled'] == true
     run_context.include_recipe('platformstack::monitors')
     # run this last because if feels so good
     run_context.include_recipe('platformstack::iptables')
