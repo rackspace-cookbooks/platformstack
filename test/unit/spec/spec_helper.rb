b27f5ce7138bb8d095b7b718ffd64907001a5d6c
Martin Smith <martin@mbs3.org>
2014-10-14 11:57:50 -0500

    Disable elkstack converge just to test platformstack

--- test/unit/spec/spec_helper.rb
+++ test/unit/spec/spec_helper.rb
@@ -14,6 +14,12 @@ Dir['./test/unit/spec/support/**/*.rb'].sort.each { |f| require f }
 
 def node_resources(node)
   node.set['newrelic']['license'] = 'dummy_value'
+
+  # need to stub this search that this causes, then set this to 'a,b,c'
+  node.set['elasticsearch']['discovery']['zen']['ping']['unicast']['hosts'] = nil
+
+  # don't want to do a huge stub for a unit test what amounts to a very simple include
+  node.set['platformstack']['elkstack_logging']['enabled'] = false
 end
 
 def stub_resources
